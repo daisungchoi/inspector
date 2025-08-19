7cb5f06cf03cd5199f21b1e12ed44c1a1da5f722
daisungchoi <daisungchoi@yahoo.com>
2025-08-19 15:01:09 -0400

    Update main.tf
    Lambda added

--- main.tf
+++ main.tf
@@ -50,14 +50,20 @@ resource "aws_iam_role_policy_attachment" "sns_publish" {
 }
 
 # 4. Lambda function (Python)
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_lambda_function" "inspector_report" {
   function_name = "inspector-weekly-report"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
 
-  filename         = "lambda.zip" # Upload package separately
-  source_code_hash = filebase64sha256("lambda.zip")
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
